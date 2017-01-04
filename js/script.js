d52f3d16337f18bf90c84914bb25d0cff95a34da
chris martinez <chris martinez>
2017-01-04 14:06:20 -0800

    add VHS logos of different colors

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// alert("Yo");
 
 
 function myArea(hi){
@@ -6,12 +5,34 @@ function myArea(hi){
   this.canvas =  document.createElement("canvas");
   this.canvas.width = 800;
   this.canvas.height = 800;
-  this.ctx = this.canvas.getContext('2d');
+  this.context = this.canvas.getContext('2d');
   document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  this.interval = setInterval(updateArea, 20);
+
 }
 
-function startVHS(){
-  var stuff = new myArea();
-  // alert("start");
+var demoArea = new myArea();
+
+function component(width, height, image, x, y){
+  // debugger;
+  var _this = this;
+  this.image  = new Image();
+  this.image.src = image;
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = demoArea.context;
+
+  this.update = function(){
+    ctx.drawImage(_this.image,
+          100, 100
+          );
+  }
+
 }
-startVHS();
+var vhsLogo = new component (100, 100, "images/vhsred.png", 0, 0);
+
+function updateArea() {
+  vhsLogo.update()
+}
